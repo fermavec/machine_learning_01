7698e3201ae74568c38ad31615c5724504b6b742
Fernando Mavec <fermavec@gmail.com>
2022-07-12 11:48:10 -0500

    PCA/IPCA libraries and models

---
+++ pca.py
@@ -0,0 +1,10 @@
+#Libraries
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+#Modules
+from sklearn.decomposition import PCA, IncrementalPCA
+from sklearn.linear_model import LogisticRegression #Classifier
+from sklearn.preprocessing import StandardScaler #Normalizer
+from sklearn.model_selection import train_test_split #Data Splitter
