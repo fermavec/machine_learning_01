d3953263154a61ace6372b0c56775f8cc5ab4262
Fernando Mavec <fermavec@gmail.com>
2022-07-12 11:55:48 -0500

    PCA/IPCA Data Loaded

--- pca.py
+++ pca.py
@@ -8,3 +8,11 @@ from sklearn.decomposition import PCA, IncrementalPCA
 from sklearn.linear_model import LogisticRegression #Classifier
 from sklearn.preprocessing import StandardScaler #Normalizer
 from sklearn.model_selection import train_test_split #Data Splitter
+
+
+#Entry Point
+if __name__ == "__main__":
+    data_heart = "./Data/heart.csv"
+    df_heart = pd.read_csv(data_heart)
+
+    print(df_heart.head(5))
