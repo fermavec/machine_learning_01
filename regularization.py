016c51489d142e12942ea82563353ab66496da88
Fernando Mavec <fermavec@gmail.com>
2022-07-12 14:29:26 -0500

    Elastic Regularization

--- regularization.py
+++ regularization.py
@@ -5,6 +5,7 @@ import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -27,6 +28,7 @@ if __name__ == "__main__":
     y_predict_linear =  modelLinear.predict(X_test)
 
     #L1 - Lasso
+    #alpha is the same as lambda, just the level of penalization
     modelLasso = Lasso(alpha=0.02).fit(X_train, y_train)
     y_predict_lasso = modelLasso.predict(X_test)
 
@@ -44,6 +46,7 @@ if __name__ == "__main__":
     ridge_loss = mean_squared_error(y_test, y_predict_ridge)
     print("Ridge Loss: ", ridge_loss)
 
+    #Coef
     print("="*32)
     print("Coef LASSO")
     print(modelLasso.coef_)
@@ -52,3 +55,17 @@ if __name__ == "__main__":
     print("Coef RIDGE")
     print(modelRidge.coef_)
 
+    #Elastic Net
+    modelElastic = ElasticNet(random_state=0).fit(X_train, y_train)
+    y_predict_elastic = modelElastic.predict(X_test)
+
+    #Loss
+    print("="*5 + "Elastic" + "="*5)
+    elastic_loss = mean_squared_error(y_test, y_predict_elastic)
+    print("Elastic Loss: ", elastic_loss)
+
+    #Coef
+    print("="*32)
+    print("Coef Elastic")
+    print(modelElastic.coef_)
+    
