34868009103c6871ce4455e7758a4d010edddfcd
Fernando Mavec <fermavec@gmail.com>
2022-07-19 10:45:48 -0500

    crossvalidation/KFolds

--- cross_validation.py
+++ cross_validation.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import (
 
 if __name__ == "__main__":
 
-    dataset = pd.read_csv('./data/felicidad.csv')
+    dataset = pd.read_csv('./Data/felicidad.csv')
 
     X = dataset.drop(['country', 'score'], axis=1)
     y = dataset['score']
